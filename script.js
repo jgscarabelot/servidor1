adff65e8753d69eca3af97b0a5299ef957913ba8
JOÃO GENTIL SCARABELOT <joao_scarabelot@estudante.sesisenai.org.br>
2025-03-31 11:21:14 -0300

    ultimo

--- script.js
+++ script.js
@@ -2,16 +2,9 @@ const http = require('http')
 const PORT = 8000
 const {soma} = require('./teste')
 const server = http.createServer((req, res) => {
-    console.log(req.url.replace('/', '').replace('?', '').split('&'))
-    let parametros = {}
-    let valores = req.url
-    for(item in valores){
-        let chaveValor = valores[item].split('=')
-        parametros[chaveValor[0]] = Number(chaveValor[1])
-    }
-    console.log(parametros)
-    let resultado = 0
-    resultado = soma(parametros.a, parametros.b)
+    const urlCapturada = url.parse(req.url,true)
+    const {query} = urlCapturada
+    let resultado = soma(Number(query.a), Number(query.b))
     res.end(O valor final é ${resultado})
 })
 
